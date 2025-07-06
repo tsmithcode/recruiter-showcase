ea7e654c09098f8ad27af7c8ffe5871604430e96
tsmithcode <thomas@tsmithcode.ai>
2025-07-06 11:54:52 -0400

    I added semicolons to missing import statements on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import ParticlesBackground from "@/components/ParticlesBackground";
 
